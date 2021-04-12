3e27564e29a5ef1bb43c833e5f92c7b1e31b2b15
novrinjr <novrinjr@gmail.com>
2021-04-12 22:52:14 +0700

    fragment

---
+++ app/src/main/java/com/example/fragmentexample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.fragmentexample;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstFragment = (Button) findViewById(R.id.firstFragment);
+        secondFragment = (Button) findViewById(R.id.secondFragment);
+
+        firstFragment.setOnClickListener(v -> loadFragment(new FirstFragment()));
+
+        secondFragment.setOnClickListener(v -> loadFragment(new SecondFragment()));
+
+
+    }
+    private void loadFragment(Fragment fragment){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
